1c11f54c06728c38f4d1f13338fe9b041a61c2b1
Pawel Zaborski <pawel.zaborski.uk@gmail.com>
2024-03-21 11:56:31 +0100

    main brackets added

--- A--currency-converter/js/calculator.js
+++ A--currency-converter/js/calculator.js
@@ -1,17 +1,19 @@
-let inputElement = document.querySelector(".js-input");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result--2");
+{
+  let inputElement = document.querySelector(".js-input");
+  let formElement = document.querySelector(".js-form");
+  let resultElement = document.querySelector(".js-result--2");
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let input = inputElement.value;
+    let input = inputElement.value;
 
-  let result = input * 5.21;
+    let result = input * 5.21;
 
-  resultElement.innerText = result.toFixed(2);
-});
+    resultElement.innerText = result.toFixed(2);
+  });
 
-formElement.addEventListener("reset", (event) => {
-  resultElement.innerText = "N/A";
-});
+  formElement.addEventListener("reset", (event) => {
+    resultElement.innerText = "N/A";
+  });
+}
